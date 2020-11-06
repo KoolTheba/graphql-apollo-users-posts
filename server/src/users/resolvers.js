a911a7030a27bc2400dfeb2e41103f50ac7eb051
Theba Gomez <gomez.teba@gmail.com>
2020-11-06 12:41:12 +0100

    feat(server): Added resolvers and api methods

---
+++ server/src/users/resolvers.js
@@ -0,0 +1,13 @@
+import { getUsers, getPosts, getComments, getUser, changeUsername } from './domain'
+
+export const resolvers = {
+  Query: {
+    getUser: (_, { userId }) => getUser(userId),
+    getUsers: () => getUsers(),
+    getPosts: (_, { userId }) => getPosts(userId),
+    getComments: (_, { postId }) => getComments(postId),
+  },
+  Mutation: {
+    changeUsername: (_, { userId, newUsername }) => changeUsername(userId, newUsername)
+  }
+}
