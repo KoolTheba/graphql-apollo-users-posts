fd8044cae06f2dadf00efbebc32cad23b3f14648
Theba Gomez <gomez.teba@gmail.com>
2020-11-06 12:42:06 +0100

    feat(): Added basic Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => (
+  <nav>
+    <section>
+      <Link to="/">Users</Link>
+    </section>
+  </nav>
+)
+
+export default Navbar
